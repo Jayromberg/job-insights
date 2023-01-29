78731284a946fe2a2214ef2ab5048b03c3252475
Jayromberg Lima Santos <jayro@pop-os.localdomain>
2023-01-29 10:28:26 -0300

    test: test_sorting.py

--- tests/sorting/test_sorting.py
+++ tests/sorting/test_sorting.py
@@ -1,5 +1,52 @@
-# from src.pre_built.sorting import sort_by
+from src.pre_built.sorting import sort_by
 
 
 def test_sort_by_criteria():
-    pass
+    dict_jobs = [
+        {
+            "name": "job1",
+            "min_salary": 3,
+            "max_salary": 4,
+            "date_posted": "01-01-2023",
+        },
+        {
+            "name": "job2",
+            "min_salary": 1,
+            "max_salary": 2,
+            "date_posted": "01-02-2023",
+        },
+    ]
+
+    sort_by(dict_jobs, "min_salary")
+
+    assert dict_jobs == [
+        {
+            "name": "job2",
+            "min_salary": 1,
+            "max_salary": 2,
+            "date_posted": "01-02-2023",
+        },
+        {
+            "name": "job1",
+            "min_salary": 3,
+            "max_salary": 4,
+            "date_posted": "01-01-2023",
+        },
+    ]
+
+    sort_by(dict_jobs, "max_salary")
+
+    assert dict_jobs == [
+        {
+            "name": "job1",
+            "min_salary": 3,
+            "max_salary": 4,
+            "date_posted": "01-01-2023",
+        },
+        {
+            "name": "job2",
+            "min_salary": 1,
+            "max_salary": 2,
+            "date_posted": "01-02-2023",
+        },
+    ]
